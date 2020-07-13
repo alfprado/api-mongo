5889b45a1bfd9d90871a47b6a734ad9618285442
André Prado <prado_fernandes@yahoo.com.br>
2020-07-12 21:04:27 -0300

    Adiciona models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
